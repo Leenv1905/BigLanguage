73f954bff1b0c6fc78c81c976e713ba65084ab0c
Lee Jung Zee <lee.nv1905@gmail.com>
2025-12-25 16:34:17 +0700

    Khởi tạo dự án

---
+++ src/main/java/com/tst/biglanguage/BigLanguageApplication.java
@@ -0,0 +1,13 @@
+package com.tst.biglanguage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigLanguageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigLanguageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tst/biglanguage/BigLanguageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tst.biglanguage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigLanguageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
